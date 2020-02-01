62d34cd5014aa523e15effa98c9cb83eacb153e2
Winston  Chung <18.chung.winston@ctkprep.org>
2020-02-01 20:13:55 +0000

    Automatically backed up by Learn

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,27 @@
+class GenresController < ApplicationController
+  def new 
+    @genre = Genre.new 
+  end 
+  
+  def create
+    @genre = Genre.create(genre_params(:name))
+  end
+  
+  def show 
+    @genre = Genre.find(params[:id])
+  end 
+  
+  def edit 
+    @genre = Genre.find(params[:id])
+  end 
+  
+  def update 
+    
+  end 
+  
+  private
+    
+    def genre_params(*args)
+      params.require(:genre).permit(*args)
+    end
+end
